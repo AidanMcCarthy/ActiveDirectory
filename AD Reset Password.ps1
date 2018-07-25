d7799c11b0dad5402b2a394bfa088fa785bf7f70
Aidan McCarthy <aidan_mccarthy@harvard.edu>
2018-07-25 16:42:44 -0400

    Initial commit

---
+++ AD Reset Password.ps1
@@ -0,0 +1,26 @@
+﻿clear-host
+WRITE-HOST 'Password change script that connects to all domains.' -foregroundcolor yellow
+WRITE-HOST 'When prompted use an administrator account/password that is consistent across all domains.' -foregroundcolor yellow
+WRITE-HOST
+
+$username=READ-HOST 'Which Username to change?'
+$newpassword = READ-HOST -assecurestring 'New Password'
+WRITE-HOST
+WRITE-HOST 'Enter an administrator account. Leave off the domain name e.g. DOMAIN\.' -foregroundcolor yellow
+$preuser = READ-HOST 'Admin Username'
+$prepass = READ-HOST -assecurestring 'Password'
+$secpassword = $prepass
+$mycreds = New-Object System.Management.Automation.PSCredential ($preuser,$secpassword)
+
+
+$data = Get-Content C:\Scripts
+
+foreach ($line in $data)
+    {
+    Set-ADAccountPassword $username -NewPassword $newpassword -Credential $mycreds -Server $line -Reset
+    WRITE-HOST $line -foregroundcolor green
+    }
+
+WRITE-HOST
+WRITE-HOST 'Password change complete.' -foregroundcolor yellow
+WRITE-HOST
---
+++ Check Replication Reg Keys.ps1
@@ -0,0 +1,8 @@
+﻿$server = Get-Content C:\temp\dc_server_list.txt
+Import-Module PSRemoteRegistry
+
+foreach ($entry in $list) {
+	Get-RegString -ComputerName $server -Hive LocalMachine -Key SYSTEM\CurrentControlSet\Services\NTDS\Parameters -Value "TCP/IP Port" | Select-Object ComputerName,Value,Data
+	Get-RegString -ComputerName $server -Hive LocalMachine -Key SYSTEM\CurrentControlSet\Services\NtFrs\Parameters -Value "RPC TCP/IP Port Assignment" | Select-Object ComputerName,Value,Data
+	Get-RegMultiString -ComputerName $server -Hive LocalMachine -Key SOFTWARE\Microsoft\Rpc\Internet -Value "Ports" | Select-Object ComputerName,Value,Data
+	}
